3c0db7fa853015a22e23bface779f4d01cd6489c
rajyesare <124474101+rajyesare@users.noreply.github.com>
2023-04-12 13:12:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,3 @@ def cube(num):
 
 result = cube(3)
 print(result)
-
-
-def hello():
-    print("Hello")
-  
-def bye():
-    print("bye")
-  
-def fnsayhi(name,age):
-    print("Hello " + name + ", you are " + str(age))
-
-fnsayhi("Raj", 35)
-fnsayhi("Priya", 30)
-
-
-def cube(num):
-    return pow(num,3)
-    print(num)
-
-result = cube(3)
-print(result)
-
-
-  
-hello()
-
-bye()
