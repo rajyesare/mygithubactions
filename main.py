5cc6eefa10defd2bc3fd58dd775f84061f4f2dcf
rajyesare <124474101+rajyesare@users.noreply.github.com>
2023-04-13 14:57:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+print("Hello World")
+
+
 def cube(num):
-    return pow(num,3)
+    return pow(num, 3)
     print(num)
+
 
 result = cube(3)
 print(result)
+
+
+def say_hello(name):
+    print(" Hello, Mr. " + name + ". How are you?")
+
+
+say_hello("Raj")
